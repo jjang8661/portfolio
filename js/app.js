a6c8d617065f79b2a978a31b5ba728a15e1c8b78
서우석 <wsseo@mouda.kr>
2017-01-09 16:18:41 -0800

    first version

---
+++ js/app.js
@@ -0,0 +1,55 @@
+//Main javascript
+'use strict';
+
+
+
+angular.module('MainApp', ['ngSanitize', 'ui.router','ui.bootstrap'])
+.config(function($stateProvider,$urlRouterProvider,$anchorScrollProvider){
+	$stateProvider
+		.state('home', {
+			url: '/', //"root" directory
+			templateUrl: 'partials/home.html'
+		})
+
+		.state('profile', {
+			url: '/profile', 
+			templateUrl: 'partials/profile.html'
+		})
+
+		.state('p1', {
+			url: '/p1',
+			templateUrl: 'partials/p1.html'
+		})
+
+		.state('p2', {
+			url: '/p2',
+			templateUrl: 'partials/p2.html'
+		})
+
+		.state('p3', {
+			url: '/p3',
+			templateUrl: 'partials/p3.html'
+		})
+
+		.state('p4', {
+			url: '/p4',
+			templateUrl: 'partials/p4.html'
+		})
+
+		.state('p5', {
+			url: '/p5',
+			templateUrl: 'partials/p5.html'
+		})
+
+		.state('p6', {
+			url: '/p6',
+			templateUrl: 'partials/p6.html'
+		})
+	$urlRouterProvider.otherwise('/');
+
+})
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -5,14 +5,11 @@ $(function() {
       'resizeDuration': 200,
       'wrapAround': true
     })
-
    $(document).on("scroll", onScroll);
- 
 })
 
 
 function onScroll(event){
-
     var scrollPos = $(document).scrollTop();
 
     if (scrollPos > 10) {
@@ -22,17 +19,4 @@ function onScroll(event){
       $('.navbar-fixed-top').removeClass("navLine");
       $('#menu-center ul img').removeClass("smallImg")
     }
-
-    $('#menu-center a').each(function () {
-        var currLink = $(this);
-        var refElement = $(currLink.attr("href"));
-        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-            $('#menu-center ul li a').removeClass("active");
-            currLink.addClass("active");
-        }
-        else{
-            currLink.removeClass("active");
-        }
-    });
-
 }
